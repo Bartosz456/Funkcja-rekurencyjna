c6debb884eaee90f4afa291de5886281ef65dba9
Bartosz456 <bartoszsz14@gmail.com>
2022-11-24 21:03:03 +0100

    funkcja rekurencyjna

---
+++ funkcja_rekurencyjna.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int potega(int n)
+{
+	int wynik=1;
+	if (n > 0) wynik = potega(n - 1)*2;
+	else wynik = 1;
+	return wynik;
+}
+int main()
+{
+	cout << potega(5);
+}
